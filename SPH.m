7031f3593b6387f19a8e08d84caead3569bbc7b9
Gabriel <gabrielbaravdish@gmail.com>
2016-10-17 15:08:30 +0200

    At least it does something. But need cleanup

--- SPH.m
+++ SPH.m
@@ -7,12 +7,37 @@ clc
 
 theta = pi/4;
 phi = pi/4;
-l = 1;
-m = 1;
+l = 0;
+m = 0;
 
 y = SH(l,m,theta,phi)
 
 %% Something
+% 1) create a grid of elevation and azimuth angles
+% help meshgrid
+% 2) calculate the radius of the desired spherical harmonic points over the grid
+% help legendre
+% 3) convert elevation, azimuth and radius to Cartesian
+% help sph2cart
+% 4) produce a 3D surface plot
+R = 1;
+Phi = linspace(0,2*pi);
+Theta = linspace(0,pi);
+[Phi,Theta] = meshgrid(Phi,Theta);
+
+for t = 1:size(Theta,2)
+    for p = 1:size(Phi,1)
+        theta = Theta(t,1);
+        phi = Phi(1,p);
+        y(t,p) = SH(l,m,theta,phi);
+    end
+end
+
+figure
+[X,Y,Z] = sph2cart(Theta,Phi,y);
+surf(X,Y,Z);
+
+
 % r = 100;
 % [X,Y,Z] = sphere(r);
 % [THETA, PHI, r] = cart2sph(X,Y,Z);
@@ -26,18 +51,3 @@ y = SH(l,m,theta,phi)
 %         y(t,p) = SH(l,m,theta,phi);
 %     end
 % end
-% 
-% 
-
-
-
-
-
-
-
-
-
-
-
-
-
