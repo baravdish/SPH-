114f1e8aa6929d5a09f018955428c699030ec8fa
Gabriel <gabrielbaravdish@gmail.com>
2016-10-17 10:06:58 +0200

    - Testing git plugin - Added comment to solve index structure for m

--- normConstantK.m
+++ normConstantK.m
@@ -1,7 +1,7 @@
 function [K] = normConstantK(l,m)
 % The index l represents the band order
 % l: band index
-% m: [-l,l] degree
+% m: [-l,l] degree OBS!! Solve index structure for m!!
 
 denominator = (2*l+1)*factorial((l-abs(m)));
 numerator = 4*pi*factorial((l+abs(m)));
